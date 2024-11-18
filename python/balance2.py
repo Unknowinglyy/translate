2e708aa157a1af5a03d5fe76874ddaff1a39ea23
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-18 16:48:37 -0600

    max out update

--- python/balance2.py
+++ python/balance2.py
@@ -16,7 +16,7 @@ MOTOR_PINS = {
 # Parameters
 CENTER_X, CENTER_Y = 2025, 2045  # Touchscreen center offsets
 BALL_DETECTION_THRESHOLD = 20    # Ball detection range
-MAX_TOTAL_STEPS = 130
+MAX_TOTAL_STEPS = 200
 angOrig = 165          # Original angle
 angToStep = 3200 / 360           # Steps per degree
 ks = 20                          # Speed amplifying constant
