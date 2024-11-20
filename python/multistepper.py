df500c88e7f009ec07ed261653095bf7605f5b2f
Blake Dejohn <unknowingly@tamu.edu>
2024-11-20 14:26:43 -0600

    no parenthesis

--- python/multistepper.py
+++ python/multistepper.py
@@ -21,7 +21,7 @@ class MultiStepper:
         longest_time = 0.0
 
         for i in range(self._num_steppers):
-            this_distance = absolute[i] - self._steppers[i].position(self)
+            this_distance = absolute[i] - self._steppers[i].position
             this_time = abs(this_distance) / self._steppers[i].max_speed()
 
             if this_time > longest_time:
