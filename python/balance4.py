aec368f894c1e482d254e7d1a5e320acbb24c7cd
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-20 17:14:04 -0600

    start w zero

--- python/balance4.py
+++ python/balance4.py
@@ -19,8 +19,8 @@ BALL_DETECTION_THRESHOLD = 20    # Ball detection range
 MAX_TOTAL_STEPS = 250
 angOrig = 170          # Original angle
 angToStep = 3200 / 360           # Steps per degree
-ks = 80                          # Speed amplifying constant
-kp, ki, kd = 4E-9, 2E-8, 7E-3    # PID constants
+ks = 20                          # Speed amplifying constant
+kp, ki, kd = 0, 0, 0    # PID constants
 
 # Kinematics parameters
 d, e, f, g = 2, 3.125, 1.75, 3.669291339
