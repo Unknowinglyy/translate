9d866668679ff5e6f8bd72734756fdb3bbe9b2ab
Blake Dejohn <unknowingly@tamu.edu>
2024-11-20 16:38:53 -0600

    running functions is good

--- accelstep/accelstep.py
+++ accelstep/accelstep.py
@@ -1,7 +1,14 @@
 import math
 import time
+
 def constrain(value, minn, maxn):
     return max(min(maxn, value), minn)
+
+def micros():
+  """
+  Mymics the Arduino micros() function.
+  """
+  return int(time.time() * 1000000)
 
 class AccelStepper:
     DIRECTION_CCW = 0
@@ -32,7 +39,176 @@ class AccelStepper:
             self.enable_outputs()
         self.set_acceleration(1)
         self.sex_max_speed(1)
+
+    def move_to(self, absolute: list):
+        if(self._targetPos != absolute):
+            self._targetPos = absolute
+            self.compute_new_speed()
     
+    def move(self, relative):
+        self.move_to(self._currentPos + relative)
+
+    def run_speed(self):
+        if(not self._stepInterval):
+            return False
+        time = micros()
+        if(time - self._lastStepTime >= self._stepInterval):
+            if(self._direction == self.DIRECTION_CW):
+                self._currentPos += 1
+            else:
+                self._currentPos -= 1
+            self.step(self._currentPos)
+            self._lastStepTime = time
+            return True
+        else:
+            return False
+    
+    def distance_to_go(self):
+        return self._targetPos - self._currentPos
+    
+    def target_position(self):
+        return self._targetPos
+    
+    def current_position(self):
+        return self._currentPos
+
+    def set_current_position(self, position):
+        self._targetPos = self._currentPos = position
+        self._n = 0
+        self._stepInterval = 0
+        self._speed = 0.0
+
+    '''
+    // Subclasses can override
+unsigned long AccelStepper::computeNewSpeed()
+{
+    long distanceTo = distanceToGo(); // +ve is clockwise from curent location
+
+    long stepsToStop = (long)((_speed * _speed) / (2.0 * _acceleration)); // Equation 16
+
+    if (distanceTo == 0 && stepsToStop <= 1)
+    {
+	// We are at the target and its time to stop
+	_stepInterval = 0;
+	_speed = 0.0;
+	_n = 0;
+	return _stepInterval;
+    }
+
+    if (distanceTo > 0)
+    {
+	// We are anticlockwise from the target
+	// Need to go clockwise from here, maybe decelerate now
+	if (_n > 0)
+	{
+	    // Currently accelerating, need to decel now? Or maybe going the wrong way?
+	    if ((stepsToStop >= distanceTo) || _direction == DIRECTION_CCW)
+		_n = -stepsToStop; // Start deceleration
+	}
+	else if (_n < 0)
+	{
+	    // Currently decelerating, need to accel again?
+	    if ((stepsToStop < distanceTo) && _direction == DIRECTION_CW)
+		_n = -_n; // Start accceleration
+	}
+    }
+    else if (distanceTo < 0)
+    {
+	// We are clockwise from the target
+	// Need to go anticlockwise from here, maybe decelerate
+	if (_n > 0)
+	{
+	    // Currently accelerating, need to decel now? Or maybe going the wrong way?
+	    if ((stepsToStop >= -distanceTo) || _direction == DIRECTION_CW)
+		_n = -stepsToStop; // Start deceleration
+	}
+	else if (_n < 0)
+	{
+	    // Currently decelerating, need to accel again?
+	    if ((stepsToStop < -distanceTo) && _direction == DIRECTION_CCW)
+		_n = -_n; // Start accceleration
+	}
+    }
+
+    // Need to accelerate or decelerate
+    if (_n == 0)
+    {
+	// First step from stopped
+	_cn = _c0;
+	_direction = (distanceTo > 0) ? DIRECTION_CW : DIRECTION_CCW;
+    }
+    else
+    {
+	// Subsequent step. Works for accel (n is +_ve) and decel (n is -ve).
+	_cn = _cn - ((2.0 * _cn) / ((4.0 * _n) + 1)); // Equation 13
+	_cn = max(_cn, _cmin); 
+    }
+    _n++;
+    _stepInterval = _cn;
+    _speed = 1000000.0 / _cn;
+    if (_direction == DIRECTION_CCW)
+	_speed = -_speed;
+
+#if 0
+    Serial.println(_speed);
+    Serial.println(_acceleration);
+    Serial.println(_cn);
+    Serial.println(_c0);
+    Serial.println(_n);
+    Serial.println(_stepInterval);
+    Serial.println(distanceTo);
+    Serial.println(stepsToStop);
+    Serial.println("-----");
+#endif
+    return _stepInterval;
+}'''
+
+    def compute_new_speed(self):
+        distance_to = self.distance_to_go()
+        steps_to_stop = (self._speed * self._speed) / (2.0 * self._acceleration)
+
+        if(distance_to == 0 and steps_to_stop <= 1):
+            self._stepInterval = 0
+            self._speed = 0.0
+            self._n = 0
+            return self._stepInterval
+        
+        if(distance_to > 0):
+            if(self._n > 0):
+                if(steps_to_stop >= distance_to or self._direction == self.DIRECTION_CCW):
+                    self._n = -steps_to_stop
+            elif(self._n < 0):
+                if(steps_to_stop < distance_to or self._direction == self.DIRECTION_CW):
+                    self._n = -self._n
+
+        elif(distance_to < 0):
+            if(self._n > 0):
+                if(steps_to_stop >= -distance_to or self._direction == self.DIRECTION_CW):
+                    self._n = -steps_to_stop
+            elif(self._n < 0):
+                if(steps_to_stop < -distance_to and self._direction == self.DIRECTION_CCW):
+                    self._n = -self._n
+        
+        if(self._n == 0):
+            self._cn = self._c0
+            self._direction = self.DIRECTION_CW if distance_to > 0 else self.DIRECTION_CCW
+        else:
+            self._cn = self._cn - ((2.0 * self._cn) / ((4.0 * self._n) + 1))
+            self._cn = max(self._cn, self._cmin)
+
+        self._n += 1
+        self._stepInterval = self._cn
+        self._speed = 1000000.0 / self._cn 
+        if(self._direction == self.DIRECTION_CCW):
+            self._speed = -self._speed
+        return self._stepInterval
+    
+
+    def run(self):
+        if(self.run_speed()):
+            self.compute_new_speed()
+        return self._speed != 0.0 or self.distance_to_go() != 0
+
     def enable_outputs(self):
         #TODO implement this
         pass
@@ -145,7 +321,6 @@ class AccelStepper:
                 self.move(stepsToStop)
             else:
                 self.move(-stepsToStop)
-
 
     def is_running(self):
         return not (self._speed == 0.0 and  self._targetPos == self._currentPos)
