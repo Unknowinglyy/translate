20571b97d36fb0ba695b53cde624ecd5ca8a2903
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-21 16:43:15 -0600

    try

--- python/balance2.py
+++ python/balance2.py
@@ -14,7 +14,7 @@ ENA = 17
 CENTER_X, CENTER_Y = 2025, 2045  # Touchscreen center offsets
 BALL_DETECTION_THRESHOLD = 20    # Ball detection range
 angOrig = 206                    # Original angle
-angToStep = 6400 / 360           # Steps per degree
+angToStep = 1600 / 360           # Steps per degree
 ks = 100                          # Speed amplifying constant
 kp, ki, kd = 4E-4, 2E-6, 7E-3    # PID constants
 
