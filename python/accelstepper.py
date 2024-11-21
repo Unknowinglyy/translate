908b58778a04d716a6b67fcad6f37174049d34a0
Blake Dejohn <unknowingly@tamu.edu>
2024-11-21 16:15:45 -0600

    back to original

--- python/accelstepper.py
+++ python/accelstepper.py
@@ -275,14 +275,14 @@ class AccelStepper:
     def step1(self, step):
         #pin[0] is step, pin[1] is direction
         #set direction first else you will get rogue pulses
-        self.set_output_pins(0b01 if self._direction else 0b00)
+        self.set_output_pins(0b10 if self._direction else 0b00)
         #step high
-        self.set_output_pins(0b11 if self._direction else 0b10)
+        self.set_output_pins(0b11 if self._direction else 0b01)
         #there is a 200ns setup time
         #delay the minimum allowed pulse width
         time.sleep(self._minPulseWidth / 1_000_000)
         #step low
-        self.set_output_pins(0b01 if self._direction else 0b00)
+        self.set_output_pins(0b10 if self._direction else 0b00)
 
     def step2(self, step):
         step = step & 0x3
