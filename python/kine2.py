37c1c20cea57a3be7dffc7a1ba835c4469a1740b
Blake Dejohn <unknowingly@tamu.edu>
2024-11-21 17:01:06 -0600

    weird that i have to do this

--- python/kine2.py
+++ python/kine2.py
@@ -45,21 +45,21 @@ class Kinematics:
         epsilon = 1e-9
 
         # Calculate angle A, B, or C
-        if leg == A:  # Leg A
+        if leg == Kinematics.A:  # Leg A
             term1 = max(epsilon, nz + 1 - nx**2)
             term2 = max(epsilon, (nz + 1) * (nz + 1 - nx**2))
             y = self.d + (self.e / 2) * (1 - (nx**2 + 3 * nz**2 + 3 * nz) / (term1 + (nx**4 - 3 * nx**2 * ny**2) / term2))
             z = hz + self.e * ny
             mag = math.sqrt(y**2 + z**2)
             angle = math.acos(max(-1, min(1, y / mag))) + math.acos(max(-1, min(1, (mag**2 + self.f**2 - self.g**2) / (2 * mag * self.f))))
-        elif leg == B:  # Leg B
+        elif leg == Kinematics.B:  # Leg B
             term1 = max(epsilon, nz + 1)
             x = (math.sqrt(3) / 2) * (self.e * (1 - (nx**2 + math.sqrt(3) * nx * ny) / term1) - self.d)
             y = x / math.sqrt(3)
             z = hz - (self.e / 2) * (math.sqrt(3) * nx + ny)
             mag = math.sqrt(x**2 + y**2 + z**2)
             angle = math.acos(max(-1, min(1, (math.sqrt(3) * x + y) / (-2 * mag)))) + math.acos(max(-1, min(1, (mag**2 + self.f**2 - self.g**2) / (2 * mag * self.f))))
-        elif leg == C:  # Leg C
+        elif leg == Kinematics.C:  # Leg C
             term1 = max(epsilon, nz + 1)
             x = (math.sqrt(3) / 2) * (self.d - self.e * (1 - (nx**2 - math.sqrt(3) * nx * ny) / term1))
             y = -x / math.sqrt(3)
