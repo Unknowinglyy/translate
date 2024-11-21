21922f755b79d2b1adb1bd02c06b2e2615e4755d
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-20 19:11:47 -0600

    pid

--- python/balance4.py
+++ python/balance4.py
@@ -20,7 +20,7 @@ MAX_TOTAL_STEPS = 250
 angOrig = 170          # Original angle
 angToStep = 6400 / 360           # Steps per degree
 ks = 20                          # Speed amplifying constant
-kp, ki, kd = 2E-2, 5E-6, 5E-7      # PID constants
+kp, ki, kd = 2E-2, 5E-4, 5E-7      # PID constants
 
 # Kinematics parameters
 d, e, f, g = 2, 3.125, 1.75, 3.669291339
