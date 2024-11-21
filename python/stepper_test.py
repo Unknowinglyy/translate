e8ff98acb2953b296cb853c07bdabe02903c571d
Blake Dejohn <unknowingly@tamu.edu>
2024-11-20 18:21:21 -0600

    not needed anymore

--- python/stepper_test.py
+++ python/stepper_test.py
@@ -4,13 +4,6 @@ import RPi.GPIO as GPIO
 import time 
 
 GPIO.setmode(GPIO.BCM)
-# GPIO.setup(23, GPIO.OUT)
-# GPIO.setup(24, GPIO.OUT)
-# GPIO.setup(20, GPIO.OUT)
-# GPIO.setup(21, GPIO.OUT)
-# GPIO.setup(5, GPIO.OUT)
-# GPIO.setup(6, GPIO.OUT)
-
 #parameters = driver type, step pin, direction pin
 stepper1 = AccelStepper(AccelStepper.DRIVER, 23, 24)
 stepper2 = AccelStepper(AccelStepper.DRIVER, 20, 21)
