d53957fbf136dc21fedea7c57a258b71e0a0abe8
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-21 16:54:31 -0600

    more sleep

--- python/balance2.py
+++ python/balance2.py
@@ -69,7 +69,7 @@ def move_to(hz, nx, ny):
     # Move all motors concurrently to the calculated positions
     multi_stepper.move_to(target_positions)
     while multi_stepper.run():
-        time.sleep(0.001)  # Allow motors to run concurrently
+        time.sleep(0.05)  # Allow motors to run concurrently
 
 def pid_control(setpoint_x, setpoint_y):
     global detected, error, integr, deriv, out, pos
