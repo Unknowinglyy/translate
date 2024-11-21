f83558f8bc003fde5ff3438055008f8e8d5a88cd
8davxgnu <davxgnu@tamu.edu>
2024-11-21 15:26:12 -0600

    Test file

--- python/multistepper.py
+++ python/multistepper.py
@@ -24,7 +24,7 @@ class MultiStepper:
         for i in range(self._num_steppers):
             this_distance = absolute[i] - self._steppers[i].current_position()
             this_time = abs(this_distance) / self._steppers[i].max_speed()
-
+            print("Distance to go: " + str(this_distance) + " Time to go: " + str(this_time))
             if this_time > longest_time:
                 longest_time = this_time
         
