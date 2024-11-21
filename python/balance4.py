31bdbc584f5900912dc5ac4128355030f4a4a11d
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-20 18:38:34 -0600

    reduce speed

--- python/balance4.py
+++ python/balance4.py
@@ -19,7 +19,7 @@ BALL_DETECTION_THRESHOLD = 20    # Ball detection range
 MAX_TOTAL_STEPS = 250
 angOrig = 170          # Original angle
 angToStep = 3200 / 360           # Steps per degree
-ks = 90                          # Speed amplifying constant
+ks = 50                          # Speed amplifying constant
 kp, ki, kd = 3, 5E-10, 5E-9      # PID constants
 
 # Kinematics parameters
