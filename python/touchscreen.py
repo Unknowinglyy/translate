ccd2b5c652cd54f7e904b318c8f0ce5325f62ad6
Blake Dejohn <unknowingly@tamu.edu>
2024-11-22 13:42:32 -0600

    comment for now

--- python/touchscreen.py
+++ python/touchscreen.py
@@ -7,9 +7,9 @@ print(board.TOUCH_XR)
 print(board.TOUCH_YD)
 print(board.TOUCH_YU)
 
-ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR, board.TOUCH_YD, board.TOUCH_YU, calibration = ((50,50), (960, 960)), size=(320, 240))
+# ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR, board.TOUCH_YD, board.TOUCH_YU, calibration = ((50,50), (960, 960)), size=(320, 240))
 
-while True:
-    p = ts.touch_point
-    if p:
-        print(p)
+# while True:
+#     p = ts.touch_point
+#     if p:
+#         print(p)
