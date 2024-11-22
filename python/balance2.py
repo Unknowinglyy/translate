095c0f0f03728c7722d653e7108785f4ce29c231
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-22 13:36:18 -0600

    reducing ki

--- python/balance2.py
+++ python/balance2.py
@@ -16,7 +16,7 @@ BALL_DETECTION_THRESHOLD = 20    # Ball detection range
 angOrig = 206                    # Original angle
 angToStep = 1600 / 360           # Steps per degree
 ks = 20                          # Speed amplifying constant
-kp, ki, kd = 4E-4, 2E-6, 7E-3    # PID constants
+kp, ki, kd = 4E-4, 2E-7, 7E-3    # PID constants
 
 # Global variables for PID control
 error = [0, 0]  # Error for X and Y axes
