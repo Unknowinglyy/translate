3b2c9b93d06d56b6f4dfc3b1f1f4be78a794fc6f
8davxgnu <davxgnu@tamu.edu>
2024-11-22 15:50:51 -0600

    TEST

--- original/TouchTest2.cpp
+++ original/TouchTest2.cpp
@@ -6,14 +6,14 @@ TouchScreen ts = TouchScreen(A1, A0, A3, A2, 0);
 void setup() {
   // Start the serial communication
   Serial.begin(9600);
-
 }
 
 void loop() {
   // Get the touch point
   TSPoint p = ts.getPoint();
 
-  if !(p.z > 0) {
+  // Check if there is a valid touch
+  if (p.z == 0) {
     Serial.print(p.x);
     Serial.print(",");
     Serial.print(p.y);
