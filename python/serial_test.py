9b50c15d9ad904d0cebe40ee5f8a00cd44062628
Blake Dejohn <unknowingly@tamu.edu>
2024-11-22 15:31:47 -0600

    more testing

--- python/serial_test.py
+++ python/serial_test.py
@@ -1,14 +1,18 @@
 import serial
 from touchScreenBasicCoordOutput import Point
+import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 def read_coords():
     try:
+        ser.write(b'S')
+        time.sleep(0.1)
         data = ser.readline().decode('utf-8').strip()
         print("got this data: " + data)
     except UnicodeDecodeError as e:
         print(f"Decoding error: {e}")
 
 if __name__ == "__main__":
-    read_coords()
+    while True:
+        read_coords()
