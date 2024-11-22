f71ac6050850d2fcc695eb90daedc027d11fc4f1
Blake Dejohn <unknowingly@tamu.edu>
2024-11-22 13:49:22 -0600

    maybe works

--- python/touchscreen.py
+++ python/touchscreen.py
@@ -1,15 +1,11 @@
 import board
 import adafruit_touchscreen
+import RPi.GPIO as GPIO
 
-# checking what the board pins are set to
-print(board.TOUCH_XL)
-print(board.TOUCH_XR)
-print(board.TOUCH_YD)
-print(board.TOUCH_YU)
+# (pin for left side of screen, pin for right side of screen, pin for bottom of screen, pin for top of screen)
+ts = adafruit_touchscreen.Touchscreen(13, 19, 16, 26, calibration = ((50,50), (960, 960)), size=(320, 240), resistance = 250)
 
-# ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR, board.TOUCH_YD, board.TOUCH_YU, calibration = ((50,50), (960, 960)), size=(320, 240))
-
-# while True:
-#     p = ts.touch_point
-#     if p:
-#         print(p)
+while True:
+    p = ts.touch_point
+    if p:
+        print(p)
