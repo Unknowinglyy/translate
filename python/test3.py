6eefb87b4271795f2e7db86e8512bf39a0395fe8
8davxgnu <davxgnu@tamu.edu>
2024-11-22 16:26:28 -0600

    TEST

--- python/test3.py
+++ python/test3.py
@@ -3,9 +3,10 @@ import serial
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    
+    # ser.reset_input_buffer()
+
     while True:
-        print(ser.in_waiting)
+        # print(ser.in_waiting)
         if ser.in_waiting > 0:
             raw_line = ser.readline()
             try:
