59e3faec7391c436fa79bccfa211a2d63072ca8b
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-22 13:34:13 -0600

    revert

--- python/balance2.py
+++ python/balance2.py
@@ -37,8 +37,8 @@ GPIO.setup(DIR_PIN, GPIO.OUT)
 
 # Initialize stepper motors
 stepper1 = AccelStepper(AccelStepper.DRIVER, 5, 6)
-stepper2 = AccelStepper(AccelStepper.DRIVER, 23, 24)
-stepper3 = AccelStepper(AccelStepper.DRIVER, 20, 21)
+stepper2 = AccelStepper(AccelStepper.DRIVER, 20, 21)
+stepper3 = AccelStepper(AccelStepper.DRIVER, 23, 24)
 
 # Configure stepper motor speeds and accelerations
 for stepper in [stepper1, stepper2, stepper3]:
