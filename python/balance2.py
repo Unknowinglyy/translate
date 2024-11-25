8649229ffeb3067b104570f75f5015303194fad4
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-25 12:53:03 -0600

    just kidding trying again

--- python/balance2.py
+++ python/balance2.py
@@ -10,7 +10,7 @@ import math
 ENA = 17
 
 # Constants and Parameters
-CENTER_X, CENTER_Y = 1000, 1000  # Touchscreen center offsets
+CENTER_X, CENTER_Y = 50, 50  # Touchscreen center offsets
 angOrig = 220                    # Original angle
 angToStep = 1500 / 360           # Steps per degree
 ks = 20                          # Speed amplifying constant
@@ -41,9 +41,9 @@ hardware wiring:
     motor 2: 20 21
     motor 3: 5 6
 '''
-stepper1 = AccelStepper(AccelStepper.DRIVER, 20, 21) 
-stepper2 = AccelStepper(AccelStepper.DRIVER, 23, 24)
-stepper3 = AccelStepper(AccelStepper.DRIVER, 5, 6)
+stepper1 = AccelStepper(AccelStepper.DRIVER, 5, 6) 
+stepper2 = AccelStepper(AccelStepper.DRIVER, 20, 21)
+stepper3 = AccelStepper(AccelStepper.DRIVER, 23, 24)
 
 # Configure stepper motor speeds and accelerations
 for stepper in [stepper1, stepper2, stepper3]:
