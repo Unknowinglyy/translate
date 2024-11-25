172f11befc107d8eba4eb9d8c8c20caba2a0f18e
8davxgnu <davxgnu@tamu.edu>
2024-11-25 17:06:17 -0600

    test

--- python/dcTesting/accelstepper.py
+++ python/dcTesting/accelstepper.py
@@ -383,9 +383,9 @@ class AccelStepper:
         if (self._targetPos == self._currentPos):
             return False
         if (self._targetPos > self._currentPos):
-            self._direction = self.DIRECTION_CW
+            self._direction = self.DIRECTION_CWW
         else:
-            self._direction = self.DIRECTION_CCW 
+            self._direction = self.DIRECTION_CW 
         return self.run_speed()
 
     def run_to_new_position(self, position):
