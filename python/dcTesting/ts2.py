72e9272fd7b19fc0b397fb7d1e5db7f556698132
8davxgnu <davxgnu@tamu.edu>
2024-11-25 17:47:08 -0600

    CREATE ts2

--- python/dcTesting/ts2.py
+++ python/dcTesting/ts2.py
@@ -16,20 +16,23 @@ def read_touch_coordinates(device_path='/dev/input/event4', timeout=1.0):
 
     # Read touch events until a valid point is captured or timeout occurs
 
-    for event in device.read_one():
-        if event.type == evdev.ecodes.EV_ABS:
-            if event.code == evdev.ecodes.ABS_X or event.code == evdev.ecodes.ABS_MT_POSITION_X:
-                samples_x.append(event.value)
-                if len(samples_x) > NUMSAMPLES:
-                    samples_x.pop(0)
-            elif event.code == evdev.ecodes.ABS_Y or event.code == evdev.ecodes.ABS_MT_POSITION_Y:
-                samples_y.append(event.value)
-                if len(samples_y) > NUMSAMPLES:
-                    samples_y.pop(0)
-            elif event.code == evdev.ecodes.ABS_PRESSURE:
-                z = event.value
+    try:
+        for event in device.read_one():
+            if event.type == evdev.ecodes.EV_ABS:
+                if event.code == evdev.ecodes.ABS_X or event.code == evdev.ecodes.ABS_MT_POSITION_X:
+                    samples_x.append(event.value)
+                    if len(samples_x) > NUMSAMPLES:
+                        samples_x.pop(0)
+                elif event.code == evdev.ecodes.ABS_Y or event.code == evdev.ecodes.ABS_MT_POSITION_Y:
+                    samples_y.append(event.value)
+                    if len(samples_y) > NUMSAMPLES:
+                        samples_y.pop(0)
+                elif event.code == evdev.ecodes.ABS_PRESSURE:
+                    z = event.value
+            
 
-            # Process samples when we have enough
+                # Process samples when we have enough
+
             if len(samples_x) == NUMSAMPLES and len(samples_y) == NUMSAMPLES:
                 # Sort samples to get the median value
                 samples_x.sort()
@@ -39,6 +42,9 @@ def read_touch_coordinates(device_path='/dev/input/event4', timeout=1.0):
 
                 # Return the point with x, y, and z values
                 return Point(x, y, z if z is not None else 0)
+    except Exception as e:
+        print(e)
+        return Point(0,0)
 
 if __name__ == "__main__":
     point = read_touch_coordinates()
