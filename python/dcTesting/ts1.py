4fbac89ea2bd3c3fcd8731255106744e4dc34b40
8davxgnu <davxgnu@tamu.edu>
2024-11-25 17:33:23 -0600

    UPDATE accwelstepper in dctesitng folder

--- python/dcTesting/ts1.py
+++ python/dcTesting/ts1.py
@@ -13,31 +13,32 @@ def read_touch_coordinates(device_path='/dev/input/event4'):
     NUMSAMPLES = 2
 
     # Read touch events in a loop
-    for event in device.read_loop():
-        if event.type == evdev.ecodes.EV_ABS:
-            if event.code == evdev.ecodes.ABS_X or event.code == evdev.ecodes.ABS_MT_POSITION_X:
-                samples_x.append(event.value)
-                if len(samples_x) > NUMSAMPLES:
-                    samples_x.pop(0)
-            elif event.code == evdev.ecodes.ABS_Y or event.code == evdev.ecodes.ABS_MT_POSITION_Y:
-                samples_y.append(event.value)
-                if len(samples_y) > NUMSAMPLES:
-                    samples_y.pop(0)
-            elif event.code == evdev.ecodes.ABS_PRESSURE:
-                z = event.value
+    while True:
+        for event in device.read_one():
+            if event.type == evdev.ecodes.EV_ABS:
+                if event.code == evdev.ecodes.ABS_X or event.code == evdev.ecodes.ABS_MT_POSITION_X:
+                    samples_x.append(event.value)
+                    if len(samples_x) > NUMSAMPLES:
+                        samples_x.pop(0)
+                elif event.code == evdev.ecodes.ABS_Y or event.code == evdev.ecodes.ABS_MT_POSITION_Y:
+                    samples_y.append(event.value)
+                    if len(samples_y) > NUMSAMPLES:
+                        samples_y.pop(0)
+                elif event.code == evdev.ecodes.ABS_PRESSURE:
+                    z = event.value
 
-            # Process samples when we have enough
-            if len(samples_x) == NUMSAMPLES and len(samples_y) == NUMSAMPLES:
-                # Sort samples to get the median value
-                samples_x.sort()
-                samples_y.sort()
-                x = samples_x[NUMSAMPLES // 2]
-                y = samples_y[NUMSAMPLES // 2]
+                # Process samples when we have enough
+                if len(samples_x) == NUMSAMPLES and len(samples_y) == NUMSAMPLES:
+                    # Sort samples to get the median value
+                    samples_x.sort()
+                    samples_y.sort()
+                    x = samples_x[NUMSAMPLES // 2]
+                    y = samples_y[NUMSAMPLES // 2]
 
-                # Return the point with x, y, and z values
-                return Point(x, y, z if z is not None else 0)
-        else:
-            return Point(0, 0)
+                    # Return the point with x, y, and z values
+                    return Point(x, y, z if z is not None else 0)
+            else:
+                return Point(0, 0)
 
 if __name__ == "__main__":
     point = read_touch_coordinates()
