7c0365460ab4a6f00a4ebf1f7b95648c1709cc6b
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-25 16:21:19 -0600

    translated points

--- python/touchScreenTranslatedCoordOutputLoop.py
+++ python/touchScreenTranslatedCoordOutputLoop.py
@@ -16,10 +16,10 @@ original_points = [
 ]
 
 translated_points = [
-    (50, 950),
+    (50, 550),
     (50, 50),
-    (950, 950),
-    (950, 50)
+    (750, 550),
+    (750, 50)
 ]
 
 # Prepare the matrices for affine transformation
