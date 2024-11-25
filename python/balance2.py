b344954ce0b94e2dfa69c0b05bd37a90b1375f46
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-25 14:49:04 -0600

    try

--- python/balance2.py
+++ python/balance2.py
@@ -41,9 +41,9 @@ hardware wiring:
     motor 2: 20 21
     motor 3: 5 6
 '''
-stepper1 = AccelStepper(AccelStepper.DRIVER, 23, 24) 
+stepper1 = AccelStepper(AccelStepper.DRIVER, 20, 21) 
 stepper2 = AccelStepper(AccelStepper.DRIVER, 5, 6)
-stepper3 = AccelStepper(AccelStepper.DRIVER, 20, 21)
+stepper3 = AccelStepper(AccelStepper.DRIVER, 23, 24)
 
 # Configure stepper motor speeds and accelerations
 for stepper in [stepper1, stepper2, stepper3]:
