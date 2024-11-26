96ba6ccb861aadbb10c473f6a1a6e320ca7f486c
Blake Dejohn <unknowingly@tamu.edu>
2024-11-25 18:21:10 -0600

    from zero to 1

--- python/balance3.py
+++ python/balance3.py
@@ -41,7 +41,7 @@ ENA = 17
 angOrig = 206.662752199
 
 #speed of the stepper motor and the speed amplifying constant
-speed = [0.0, 0.0, 0.0]
+speed = [1.0, 1.0, 1.0]
 speedPrev = [0.0, 0.0, 0.0]
 ks = float(20)
 
