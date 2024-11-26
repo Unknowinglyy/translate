c115e0c49b46ab1fa6a4a178a2952556294936c8
8davxgnu <davxgnu@tamu.edu>
2024-11-25 19:29:15 -0600

    ts

--- python/dcTesting/b3Snip_Ard.py
+++ python/dcTesting/b3Snip_Ard.py
@@ -207,7 +207,7 @@ def PID(setpointX, setpointY):
 
     # continues moving platform and waits until 20 milliseconds have elapsed
     timeI = millis() # Convert to milliseconds
-    while (millis() - timeI < 10): # DC - changed from 20 to 5
+    while (millis() - timeI < 25): 
         moveTo(4.25, -out[0], -out[1])  # moves the platform
 
 def loop():
