4bc47a545562fde9711a24b1b9fae030cce41019
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-30 12:03:36 -0600

    changing back pid

--- python/balance2.py
+++ python/balance2.py
@@ -12,9 +12,9 @@ ENA = 17
 # Constants and Parameters
 CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
-angToStep = 3200 / 360           # Steps per degree
+angToStep = 1100 / 360           # Steps per degree
 ks = 20                          # Speed amplifying constant
-kp, ki, kd = 4E-4, 2E-6, 7E-3    # PID constants
+kp, ki, kd = .00035, 0, 0    # PID constants
 
 # Global variables for PID control
 error = [0, 0]  # Error for X and Y axes
@@ -51,8 +51,8 @@ stepper3 = AccelStepper(AccelStepper.DRIVER, 23, 24)
 
 # Configure stepper motor speeds and accelerations
 for stepper in [stepper1, stepper2, stepper3]:
-    stepper.set_max_speed(1000)  # Adjust as needed
-    stepper.set_acceleration(1000)  # Adjust as needed
+    stepper.set_max_speed(8000)  # Adjust as needed
+    stepper.set_acceleration(8000)  # Adjust as needed
 
 # Create a MultiStepper instance
 multi_stepper = MultiStepper()
