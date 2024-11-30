826480453017d108e40b8df340247683e1a2b0cf
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-29 23:01:33 -0600

    lowering speed

--- python/balance2.py
+++ python/balance2.py
@@ -51,8 +51,8 @@ stepper3 = AccelStepper(AccelStepper.DRIVER, 23, 24)
 
 # Configure stepper motor speeds and accelerations
 for stepper in [stepper1, stepper2, stepper3]:
-    stepper.set_max_speed(5000)  # Adjust as needed
-    stepper.set_acceleration(5000)  # Adjust as needed
+    stepper.set_max_speed(1000)  # Adjust as needed
+    stepper.set_acceleration(1000)  # Adjust as needed
 
 # Create a MultiStepper instance
 multi_stepper = MultiStepper()
