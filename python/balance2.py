7863ca08b821984c5e306c1582022a338ecf31f1
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-30 13:24:29 -0600

    testing kp

--- python/balance2.py
+++ python/balance2.py
@@ -12,9 +12,9 @@ ENA = 17
 # Constants and Parameters
 CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
-angToStep = 1400 / 360           # Steps per degree
+angToStep = 1300 / 360           # Steps per degree
 ks = 25                          # Speed amplifying constant
-kp, ki, kd = .000393, 0, 0    # PID constants
+kp, ki, kd = .00036, 0, 0    # PID constants
 
 # Global variables for PID control
 error = [0, 0]  # Error for X and Y axes
