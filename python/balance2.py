4d9ec35edaee36ae82a39534228c3e3a16457e18
8davxgnu <davxgnu@tamu.edu>
2024-11-30 17:37:54 -0600

    b7

--- python/balance2.py
+++ python/balance2.py
@@ -12,7 +12,7 @@ ENA = 17
 # Constants and Parameters
 CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
-angToStep = 800 / 360           # Steps per degree
+angToStep = 1100 / 360           # Steps per degree
 ks = 30                         # Speed amplifying constant
 kp, ki, kd = .00034, 0, 0.0007    # PID constants
 
