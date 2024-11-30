b9404326afd78bf8785a310e054db72277e0c9b5
8davxgnu <davxgnu@tamu.edu>
2024-11-30 17:16:14 -0600

    b6

--- python/balance6.py
+++ python/balance6.py
@@ -121,7 +121,7 @@ def pid_control(setpoint_x, setpoint_y):
 def balance_ball():
     move_to(4.25, 0, 0)
     print("Setting up motor offsets...")
-    multi_stepper.move_to([400, 0, 400])
+    multi_stepper.move_to([100, 0, 100])
     multi_stepper.run_speed_to_position()
     time.sleep(2)
     debug_log("Starting balance loop...")
