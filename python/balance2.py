04ef43b51fd55c7a877e300d3a5a55b610bdf48c
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-30 14:43:06 -0600

    testing ang

--- python/balance2.py
+++ python/balance2.py
@@ -12,7 +12,7 @@ ENA = 17
 # Constants and Parameters
 CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
-angToStep = 1400 / 360           # Steps per degree
+angToStep = 600 / 360           # Steps per degree
 ks = 25                         # Speed amplifying constant
 kp, ki, kd = .00034, 0, 0    # PID constants
 
