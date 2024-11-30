bb9d655a171d4e21af5766e519c50f1f942c10e5
8davxgnu <davxgnu@tamu.edu>
2024-11-30 16:51:28 -0600

    b2

--- python/balance2.py
+++ python/balance2.py
@@ -14,7 +14,7 @@ CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
 angToStep = 1800 / 360           # Steps per degree
 ks = 80                         # Speed amplifying constant
-kp, ki, kd = .00029, 0, 0.05    # PID constants
+kp, ki, kd = .00034, 0, 0.004    # PID constants
 
 # Global variables for PID control
 error = [0, 0]  # Error for X and Y axes
