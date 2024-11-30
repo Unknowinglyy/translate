b9da65f238e02abb9f5ca92b9f02021c1fc8b4a6
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-11-30 14:46:41 -0600

    testing ks

--- python/balance2.py
+++ python/balance2.py
@@ -13,7 +13,7 @@ ENA = 17
 CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
 angToStep = 3200 / 360           # Steps per degree
-ks = 15                         # Speed amplifying constant
+ks = 50                         # Speed amplifying constant
 kp, ki, kd = .00034, 0, 0    # PID constants
 
 # Global variables for PID control
