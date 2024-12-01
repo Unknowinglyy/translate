4ee0d6d0a37875bdf8d2803f1b4b30b8493bd017
Jocelyn Dai <jocelyn.dai@tamu.edu>
2024-12-01 11:06:17 -0600

    testing

--- python/balance2.py
+++ python/balance2.py
@@ -14,7 +14,7 @@ CENTER_X, CENTER_Y = 500, 500  # Touchscreen center offsets
 angOrig = 206                    # Original angle
 angToStep = 1100 / 360           # Steps per degree
 ks = 30                         # Speed amplifying constant
-kp, ki, kd = .0004, 0.000005, 0.0007    # PID constants
+kp, ki, kd = .0004, 0.000005, 0.001    # PID constants
 
 # Global variables for PID control
 error = [0, 0]  # Error for X and Y axes
